821f00643ff3849a1894d19aaf22d6b7e5753a6c
IaroslavKr <iaroslavkr1104@gmail.com>
2026-08-23 20:35:52 +0900

    Замена конструктора, добавление зависимости

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/server/mapper/EndpointHitMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/server/mapper/EndpointHitMapper.java
@@ -1,12 +1,12 @@
 package ru.practicum.ewm.stats.server.mapper;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import ru.practicum.ewm.stats.dto.EndpointHitDto;
 import ru.practicum.ewm.stats.server.model.EndpointHit;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class EndpointHitMapper {
-    private EndpointHitMapper() {
-    }
-
     public static EndpointHit toEndpointHit(EndpointHitDto hitDto) {
         return new EndpointHit(
                 null,
